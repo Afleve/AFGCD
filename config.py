46e8be4d43a42550fdd4c2640eb653576f1e0adc
Afleve <graceafleve@gmail.com>
2025-02-17 20:15:17 +0800

    update

---
+++ config.py
@@ -0,0 +1,19 @@
+# -----------------
+# DATASET ROOTS
+# -----------------
+DATASET_DIR = ''
+cifar_10_root = '/data/Public/cifar10'
+cifar_100_root = '/data/Public/cifar100'
+cub_root = '/data/Public/'
+aircraft_root = '/data/Public/fgvc-aircraft'
+car_root = '/data/Public/stanford_cars'
+herbarium_dataroot = '/data/Public/herbarium_19'
+imagenet_root = '/data/Public/imagenet/'
+
+# OSR Split dir
+osr_split_dir = 'data/ssb_splits'
+
+# -----------------
+# OTHER PATHS
+# -----------------
+exp_root = 'dev_outputs' # All logs and checkpoints will be saved here
---
+++ models/TIME.py
@@ -0,0 +1,52 @@
+import torch
+import torch.nn as nn
+from timm.layers.mlp import Mlp
+from timm.models.layers import trunc_normal_
+from timm.layers.weight_init import trunc_normal_tf_
+
+class Simple_Cross_Attention(nn.Module):
+    def __init__(self, feat_dim=768, mlp_ratio=4.0):
+        super().__init__()
+        
+        self.D_sqrt = feat_dim**-0.5
+        self.learnable_query = nn.Parameter(torch.empty(1, feat_dim))
+        self.mlp_norm = nn.LayerNorm(feat_dim, eps=1e-6)
+        self.mlp = Mlp(feat_dim, int(feat_dim * mlp_ratio))
+
+        trunc_normal_tf_(self.learnable_query, std=self.D_sqrt)
+
+    def forward(self, x):
+        B = x.shape[0]
+        # QKV
+        q = self.learnable_query.repeat(B, 1).unsqueeze(1)
+        k, v = x, x
+        attn = q @ k.transpose(-2, -1)
+        attn_sm = (attn * self.D_sqrt).softmax(dim=-1)
+        x = (attn_sm @ v).squeeze(1)
+        # FFN
+        x = x + self.mlp(self.mlp_norm(x))
+        return x, attn.squeeze(1)
+
+class Aux_Head(nn.Module):
+    def __init__(self, feat_dim, num_classes):
+        super().__init__()
+        self.norm = nn.LayerNorm(feat_dim)
+        self.head = nn.Linear(feat_dim, num_classes)
+        trunc_normal_(self.head.weight, std=0.02)
+        self.head.bias.data.zero_()
+
+    def forward(self, x):
+        x = self.norm(x)
+        x = self.head(x)
+        return x
+
+class Token_Importance_Measurer(nn.Module):
+    def __init__(self, num_classes=1000, feat_dim=768):
+        super().__init__()
+        self.sim_cross_attn = Simple_Cross_Attention(feat_dim=feat_dim)
+        self.aux_head = Aux_Head(feat_dim, num_classes)
+
+    def forward(self, x):
+        weighted_feat, attn = self.sim_cross_attn(x)
+        pred = self.aux_head(weighted_feat)
+        return attn, pred, weighted_feat
